6ebf33c927bb9b9c5740429f9c24086d7d15fb6e
Oreoluwa Jaiyesimi <orejay@yahoo.com>
2025-11-18 18:15:23 +0100

    chore(main): add ad script

--- src/Main.js
+++ src/Main.js
@@ -4,45 +4,45 @@ import { useMediaQuery } from "@mui/material";
 const Footer = lazy(() => import("./components/Footer"));
 
 const Main = ({ Prop, logIn, nav }) => {
-  // useEffect(() => {
-  //   const script = document.createElement("script");
-  //   script.id = "clever-core";
-  //   script.setAttribute("data-cfasync", "false");
-  //   script.type = "text/javascript";
-  //   script.async = true;
-  //   script.defer = true;
-  //   script.innerHTML = `
-  //     /* <![CDATA[ */
-  //                                   (function (document, window) {
-  //                                       var a, c = document.createElement("script"), f = window.frameElement;
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.id = "clever-core";
+    script.setAttribute("data-cfasync", "false");
+    script.type = "text/javascript";
+    script.async = true;
+    script.defer = true;
+    script.innerHTML = `
+      /* <![CDATA[ */
+                                    (function (document, window) {
+                                        var a, c = document.createElement("script"), f = window.frameElement;
 
-  //                                       c.id = "CleverCoreLoader50448";
-  //                                       c.src = "https://scripts.cleverwebserver.com/1b246c2b83b7f322480a19abdd2ceff6.js";
+                                        c.id = "CleverCoreLoader50448";
+                                        c.src = "https://scripts.cleverwebserver.com/1b246c2b83b7f322480a19abdd2ceff6.js";
 
-  //                                       c.async = !0;
-  //                                       c.type = "text/javascript";
-  //                                       c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
-  //                                       c.setAttribute("data-callback", "put-your-callback-macro-here");
+                                        c.async = !0;
+                                        c.type = "text/javascript";
+                                        c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
+                                        c.setAttribute("data-callback", "put-your-callback-macro-here");
 
-  //                                       try {
-  //                                           a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
-  //                                       } catch (e) {
-  //                                           a = !1;
-  //                                       }
+                                        try {
+                                            a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
+                                        } catch (e) {
+                                            a = !1;
+                                        }
 
-  //                                       a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
-  //                                       a.parentNode.insertBefore(c, a);
-  //                                   })(document, window);
-  //                                   /* ]]> */
-  //   `;
+                                        a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
+                                        a.parentNode.insertBefore(c, a);
+                                    })(document, window);
+                                    /* ]]> */
+    `;
 
-  //   document.body.appendChild(script);
+    document.body.appendChild(script);
 
-  //   return () => {
-  //     // Clean up function to remove the script when the component unmounts
-  //     document.body.removeChild(script);
-  //   };
-  // }, []);
+    return () => {
+      // Clean up function to remove the script when the component unmounts
+      document.body.removeChild(script);
+    };
+  }, []);
 
   useEffect(() => {
     const script = document.createElement("script");
